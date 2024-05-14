fdb87b9880ce46ca45c647309681918fbae2c72b
JohnD0e1 <johnydo448@gmail.com>
2024-05-14 16:26:35 -0600

    roiuting done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from "./Components/Home";
 import About from "./Components/About"; 
 import Resume from "./Components/Resume"; 
 import Contact from "./Components/Contact";
-import MotivProject from "./Projects/Motiv"; 
-import HRBlockProject from "./Projects/HRB"; 
+import MotivProject from "./Projects/MotivProject"; 
+import HRBlockProject from "./Projects/HRBlockProject"; 
 
 
 function App() {
@@ -17,8 +17,8 @@ function App() {
         <Route path = "/About" element = {<About />} />
         <Route path = "/Resume" element = {<Resume />} />
         <Route path = "/Contact" element = {<Contact />} />
-        <Route path = "/MotivProject" element = {<Contact />} />
-        <Route path = "/HRBlockProject" element = {<Contact />} />
+        <Route path = "/MotivProject" element = {<MotivProject />} />
+        <Route path = "/HRBlockProject" element = {<HRBlockProject />} />
       </Routes>
     </div>
   );
