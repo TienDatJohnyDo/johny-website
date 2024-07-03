98b2571ae7b0f941033af5a88ccfc172cc209c82
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-07-03 14:46:55 -0600

    Removed Contacts to Scroll to View

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from "./Components/Home"; 
 import About from "./Components/About"; 
-import Resume from "./Components/Resume"; 
-import Contact from "./Components/Contact";
+import Resume from "./Components/Resume";
 import MotivProject from "./Projects/MotivProject"; 
 import HRBlockProject from "./Projects/HRBlockProject"; 
 
@@ -16,7 +15,6 @@ function App() {
         <Route path = "/" element = {<Home />} />
         <Route path = "/About" element = {<About />} />
         <Route path = "/Resume" element = {<Resume />} />
-        <Route path = "/Contact" element = {<Contact />} />
         <Route path = "/MotivProject" element = {<MotivProject />} />
         <Route path = "/HRBlockProject" element = {<HRBlockProject />} />
       </Routes>
